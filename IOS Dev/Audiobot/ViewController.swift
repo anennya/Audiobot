5998d53aec2b492b3eb0193f74aa49955efcfc9d
Arjun <arjun@foursquare.com>
2015-09-05 08:51:13 -0700

    Adding ios dev

---
+++ IOS Dev/Audiobot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Audiobot
+//
+//  Created by Arjun Sundararajan on 9/5/15.
+//  Copyright (c) 2015 Arjun Sundararajan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
