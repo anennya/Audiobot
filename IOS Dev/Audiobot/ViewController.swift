e606ea8655cba4bb51098ee76998ebdba0fe5ea4
Arjun <arjun@foursquare.com>
2015-09-08 07:06:56 -0700

    Added Swifter to embedded framework

--- IOS Dev/Audiobot/ViewController.swift
+++ IOS Dev/Audiobot/ViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 import AVFoundation
+import SwifteriOS
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
+    
+    @IBAction func textToSpeech(sender: UIButton) {
+        myUtterance =  AVSpeechUtterance(string: textView.text)
+        myUtterance.rate = 0.2
+        synth.speakUtterance(myUtterance)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ IOS Dev/AudiobotFramework/AudiobotFramework.h
@@ -0,0 +1,19 @@
+//
+//  AudiobotFramework.h
+//  AudiobotFramework
+//
+//  Created by Arjun Sundararajan on 9/8/15.
+//  Copyright (c) 2015 Arjun Sundararajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudiobotFramework.
+FOUNDATION_EXPORT double AudiobotFrameworkVersionNumber;
+
+//! Project version string for AudiobotFramework.
+FOUNDATION_EXPORT const unsigned char AudiobotFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudiobotFramework/PublicHeader.h>
+
+
