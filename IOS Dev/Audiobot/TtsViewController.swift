d43ec20aff4d9787c7b1aea3b99954053950b363
Arjun <arjun@foursquare.com>
2015-09-14 07:28:04 -0700

    Stripped URLs off the text Audiobot says. And added username to the text

--- IOS Dev/Audiobot/TtsViewController.swift
+++ IOS Dev/Audiobot/TtsViewController.swift
@@ -17,6 +17,9 @@ class TtsViewController: UIViewController {
     var tweets : [JSONValue] = []
     let synth = AVSpeechSynthesizer()
     var myUtterance = AVSpeechUtterance(string: "")
+    @IBOutlet weak var speakBtn: UIButton!
+    @IBOutlet weak var stopBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,23 +33,67 @@ class TtsViewController: UIViewController {
     }
     
    
+    @IBAction func PauseSpeech(sender: UIButton) {
+        synth.pauseSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+        self.animateActionButtonAppearance(false)
+    }
+    
+    
+    @IBAction func StopSpeech(sender: UIButton) {
+        synth.stopSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+        self.animateActionButtonAppearance(false)
+    }
+    
     @IBAction func TextToSpeech(sender: UIButton) {
-        for tweet in tweets{
-            myUtterance = AVSpeechUtterance(string: tweet["text"].string)
-            myUtterance.rate = 0.1
-            synth.speakUtterance(myUtterance)
-
+        var actualText = ""
+        var toSpeak = ""
+        if !synth.speaking{
+        
+            for tweet in tweets{
+                let text = tweet["text"]
+                let user = tweet["user"]["name"]
+                
+                if let myString = text.string {
+                    var myArray : [String] = myString.componentsSeparatedByString("http")
+                    println(myArray)
+                    actualText = myArray[0] 
+                }
+                else {
+                    continue
+                }
+                
+                if let myString = user.string {
+                    toSpeak = myString + "  said " + actualText
+                }
+                    myUtterance = AVSpeechUtterance(string: toSpeak)
+                    myUtterance.rate = 0.05
+                    synth.speakUtterance(myUtterance)
+            }
         }
+        else{
+            synth.continueSpeaking()
+        }
+        self.animateActionButtonAppearance(true)
+
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        
+        func animateActionButtonAppearance(shouldHideSpeakButton: Bool) {
+            var speakButtonAlphaValue: CGFloat = 1.0
+            var pauseStopButtonsAlphaValue: CGFloat = 0.0
+            
+            if shouldHideSpeakButton {
+                speakButtonAlphaValue = 0.0
+                pauseStopButtonsAlphaValue = 1.0
+            }
+            
+            UIView.animateWithDuration(0.25, animations: { () -> Void in
+                self.speakBtn.alpha = speakButtonAlphaValue
+                
+                self.pauseBtn.alpha = pauseStopButtonsAlphaValue
+                
+                self.stopBtn.alpha = pauseStopButtonsAlphaValue
+            })
+        }
+        
+ 
 }
